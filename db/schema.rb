7bc05ad6522d45318dd46cbdb4157f786ac3e087
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 12:08:45 +0545

    new table personal and office contact is created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219060002) do
+ActiveRecord::Schema.define(version: 20150219062117) do
 
   create_table "office_contacts", force: :cascade do |t|
     t.integer  "user_id"
