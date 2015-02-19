2c0ed45b8f324240db92008ad49915bfdbefda9e
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 13:22:16 +0545

    created table contacts

--- db/migrate/20150219071951_create_contacts.rb
+++ db/migrate/20150219071951_create_contacts.rb
@@ -1,6 +1,12 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      t.integer :user_id
+      t.integer :personal_detail_id
+      t.integer :office_detail_id
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
