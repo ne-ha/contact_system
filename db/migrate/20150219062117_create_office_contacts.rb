360cf873102c6b59086a1bd7871b4569feb2d1b2
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 12:08:29 +0545

    office contacts table is created

--- db/migrate/20150219062117_create_office_contacts.rb
+++ db/migrate/20150219062117_create_office_contacts.rb
@@ -1,7 +1,12 @@
 class CreateOfficeContacts < ActiveRecord::Migration
   def change
     create_table :office_contacts do |t|
-
+      t.integer :user_id
+      t.integer :phone_id
+      t.string :office_name
+      t.string :office_address
+      t.string :office_email
+      t.string :office_website
       t.timestamps null: false
     end
   end
