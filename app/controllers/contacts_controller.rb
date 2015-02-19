497e4ce01c1603a1d7aeee3925b51910054c15eb
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 17:27:22 +0545

    added create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,16 +12,23 @@ class ContactsController < ApplicationController
 
   def new
     @contact = current_user.contacts.new
+  end
+
+  def create
+    @contact = current_user.contacts.create(contact_param)
     respond_to do |format|
       format.html {redirect_to root_path}
       format.js
     end
   end
 
-  def create
-  end
-
   def edit
   end
+
+  private
+  
+    def contact_param
+      params.require(:contact).permit(:first_name, :last_name)
+    end
 
 end
