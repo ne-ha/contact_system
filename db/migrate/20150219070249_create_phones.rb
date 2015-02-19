a26767b5e34560da008a1865ea38db7b190dcb42
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 13:21:53 +0545

    created table phones

--- db/migrate/20150219070249_create_phones.rb
+++ db/migrate/20150219070249_create_phones.rb
@@ -1,6 +1,10 @@
 class CreatePhones < ActiveRecord::Migration
   def change
     create_table :phones do |t|
+      t.integer :personal_detail_id
+      t.integer :office_detail_id
+      t.string :cell_phone
+      t.string :landline
 
       t.timestamps null: false
     end
