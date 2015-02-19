5e54515872557077b785634721818099a5ffaef8
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 13:22:28 +0545

    created table personal details

--- db/migrate/20150219072906_create_personal_details.rb
+++ db/migrate/20150219072906_create_personal_details.rb
@@ -1,6 +1,11 @@
 class CreatePersonalDetails < ActiveRecord::Migration
   def change
     create_table :personal_details do |t|
+      t.integer :contact_id
+      t.integer :phone_id
+      t.string :address
+      t.string :email
+      t.string :website
 
       t.timestamps null: false
     end
