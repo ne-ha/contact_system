e744bb0f189b087b5854f26c5fe2686aeae80674
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 13:22:45 +0545

    created table office details

--- db/migrate/20150219072918_create_office_details.rb
+++ db/migrate/20150219072918_create_office_details.rb
@@ -1,6 +1,12 @@
 class CreateOfficeDetails < ActiveRecord::Migration
   def change
     create_table :office_details do |t|
+      t.integer :contact_id
+      t.integer :phone_id
+      t.string :office_name
+      t.string :office_address
+      t.string :office_email
+      t.string :office_website
 
       t.timestamps null: false
     end
