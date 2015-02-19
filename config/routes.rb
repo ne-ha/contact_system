15f3b04f048c67c5227d1017fdc3f0b262f24178
NEHA SUWAl <neha@jyaasa.com>
2015-02-19 13:05:41 +0545

    generated new controller and model contact

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
+  resources :contacts
+  
   resources :personal_contacts
 
   resources :office_contacts
