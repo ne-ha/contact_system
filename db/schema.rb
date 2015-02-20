4803aff43add943f273b6f9ac814e3aed95bb38f
NEHA SUWAl <neha@jyaasa.com>
2015-02-20 14:42:03 +0545

    contact table is modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219072918) do
+ActiveRecord::Schema.define(version: 20150220085542) do
 
   create_table "contacts", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "personal_detail_id"
-    t.integer  "office_detail_id"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "office_details", force: :cascade do |t|
