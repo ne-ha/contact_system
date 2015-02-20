f60105024ced189640a00537ee9286861391af37
NEHA SUWAl <neha@jyaasa.com>
2015-02-20 13:47:03 +0545

    removed instance variable

--- app/controllers/personal_details_controller.rb
+++ app/controllers/personal_details_controller.rb
@@ -3,8 +3,6 @@ class PersonalDetailsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @personal_details = current_user.contacts.personal_details
-    redirect_to
   end
 
   def new
