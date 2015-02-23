46e325be1ac80410755ad2731fc56c6d8fbba4a0
NEHA SUWAl <neha@jyaasa.com>
2015-02-23 11:16:48 +0545

    schema for phones table is updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223051545) do
+ActiveRecord::Schema.define(version: 20150223052746) do
 
   create_table "contacts", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,12 +41,12 @@ ActiveRecord::Schema.define(version: 20150223051545) do
   end
 
   create_table "phones", force: :cascade do |t|
-    t.string   "cell_phone"
-    t.string   "landline"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "office_detail_id"
     t.integer  "personal_detail_id"
+    t.text     "landline"
+    t.text     "cell_phone"
   end
 
   create_table "users", force: :cascade do |t|
