f0ed646c2cf30e71b4d1385aac39d399ce9c5334
NEHA SUWAl <neha@jyaasa.com>
2015-02-24 15:04:37 +0545

    added test example and commented built in examples

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -2,30 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #delete" do
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
+      get :delete
       expect(response).to have_http_status(:success)
     end
   end
