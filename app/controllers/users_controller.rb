33d50e76de1b43e0a0ce259cb2561a67f093e5b1
NEHA SUWAl <neha@jyaasa.com>
2015-02-24 14:59:03 +0545

    added before action and added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
     
   def index
-    @contacts = current_user.contacts
-    #@personal_details = current_user.personal_details
-    #@office_details = current_user.office_details
+    @contacts = Kaminari.paginate_array(current_user.contacts).page(params[:page]).per(10)
   end  
   
 end
