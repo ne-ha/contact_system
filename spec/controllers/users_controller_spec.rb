5ae31323aff84c0c3a0220c54716d7d53805f938
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 16:19:02 +0545

    added test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  before :each do
+    User.make(:email => 'user@example.com' , :password => 'password')
+  end
+
+  # describe "#index" do
+  #   it "should be success" do
+  #     get :index
+  #     expect(response).to be_success
+  #   end
+  # end
+  it "signs me in" do
+    visit "/users/sign_in"
+    within("#users") do
+      fill_in 'Email' , :with => 'user@example.com'
+      fill_in 'Password' , :with => 'password'
     end
+    click_button 'Sign in'
+    expect(page).to have_content 'Success'
   end
 
 end
