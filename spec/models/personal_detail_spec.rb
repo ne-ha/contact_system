f6dfb25e5bb3a6804dff63ce9dd1d8c006f8cbfd
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 11:15:58 +0545

    added test for valid email address and unique email address

--- spec/models/personal_detail_spec.rb
+++ spec/models/personal_detail_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PersonalDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have valid email address" do
+    personal_detail = PersonalDetail.new(
+        email: 'test@example.com')
+      expect(personal_detail).to be_valid
+  end
+
+  it "is invalid with a duplicate email" do
+    PersonalDetail.create(
+      email: 'test@example.com')
+    personal_detail = PersonalDetail.new(email: 'test@example.com')
+    personal_detail.valid?
+    expect(personal_detail).to be_invalid
+  end
 end
