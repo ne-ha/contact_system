9f9617ee128f5e63ea78a4bec249e72f70c0fa83
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 14:39:50 +0545

    removed test

--- spec/models/personal_detail_spec.rb
+++ spec/models/personal_detail_spec.rb
@@ -18,19 +18,4 @@ RSpec.describe PersonalDetail, type: :model do
       website:'www.esss.com')
     expect(personal).to be_invalid
   end
-
-  it " two contacts can have same address" do
-    contact = Contact.create(first_name: 'Nitish', last_name: 'Shakya')
-    contact.create_personal_detail( 
-      address: 'Kuleshwore' , 
-      email: 'ester@example.com', 
-      website:'www.ss.com')
-    Contact.new(first_name: 'Kinara', last_name: 'Maharjan')
-    personal = contact.create_personal_detail( 
-      address: 'Tinkune' , 
-      email: 'es@work.com', 
-      website:'www.esss.com')
-    expect(personal).to be_valid
-  end
-
 end
