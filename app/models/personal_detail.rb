09f49c5ed7dba5cc2275363a39f631ada2eacb8a
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 13:43:43 +0545

    removed validation uniqueness

--- app/models/personal_detail.rb
+++ app/models/personal_detail.rb
@@ -6,7 +6,6 @@ class PersonalDetail < ActiveRecord::Base
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i 
   
   validates :email, :length => { :maximum => 100 },
-                    :format => EMAIL_REGEX,
-                    :uniqueness => true
+                    :format => EMAIL_REGEX
 
 end
