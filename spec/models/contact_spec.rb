0f9c69a3f1932fce72f023485512d37fbb634315
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 10:53:59 +0545

    added test for two contacts with same firstname

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe Contact, type: :model do
       contact = Contact.new(first_name: 'Anil', last_name: 'Shrestha')
       expect(contact.full_name).to eq 'Anil Shrestha'
     end
+
+    it "can have two contacts with same first name" do
+      Contact.create(
+        first_name: 'Sam',
+        last_name: 'Shrestha')
+      contact = Contact.new(
+        first_name: 'Sam',
+        last_name: 'Tamang')
+      expect(contact).to be_valid
+    end
   end 
 
 end
