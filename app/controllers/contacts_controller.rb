e8b83695d55ace13fb29d8a322abea554306a611
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 11:14:28 +0545

    modified create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = current_user.contacts.create(contact_param[:contact])
+    @contact = current_user.contacts.create(basic_param)
     @personal_detail = @contact.create_personal_detail(contact_param[:personal_detail])
     @office_detail = @contact.create_office_detail(contact_param[:office_detail])
     @personal_phone = @personal_detail.phones.create(contact_param[:personal_phone])
