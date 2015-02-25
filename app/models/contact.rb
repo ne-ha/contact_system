4420f3ca8eb973b27456a28a0d4b0bf82391a3de
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 10:25:37 +0545

    scope is added to sort alphabetically

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,8 @@ class Contact < ActiveRecord::Base
   belongs_to :user
   has_one :personal_detail
   has_one :office_detail
+
+  scope :alphabetically, lambda{("contacts.full_name ASC")}
   
   def full_name
     [first_name, last_name].join(" ")
