0e4cfc1708ed738089d8111a937c54365a232ae2
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 09:31:26 +0545

    added examples to test for users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe User do
+    it "is valid with email and password" do
+      user = User.new(
+        email: 'test@example.com',
+        password: 'secretpassword')
+      expect(user).to be_valid
+    end
+
+    it "is invalid without an email" do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user).to be_invalid
+    end
+  end
+
 end
