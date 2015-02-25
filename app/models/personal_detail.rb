0a11c46d0bf5a0d3e5678af51394f48ab0cd303c
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 11:14:46 +0545

    added validation for email

--- app/models/personal_detail.rb
+++ app/models/personal_detail.rb
@@ -2,5 +2,11 @@ class PersonalDetail < ActiveRecord::Base
 
   belongs_to :contact
   has_many :phones
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i 
   
+  validates :email, :length => { :maximum => 100 },
+                    :format => EMAIL_REGEX,
+                    :uniqueness => true
+
 end
