02a3e1d89ee4b74236e2887a277241463b342ba4
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 14:39:33 +0545

    removed unnecessary test

--- spec/models/office_detail_spec.rb
+++ spec/models/office_detail_spec.rb
@@ -10,20 +10,4 @@ RSpec.describe OfficeDetail, type: :model do
       office_website:'www.aske.com')
     expect(office).to be_valid
   end
-
-  it " two contacts can have same address" do
-    contact =   Contact.create(first_name: 'Nitish', last_name: 'Shakya')
-    contact.create_office_detail( 
-      office_name: 'Technologies',
-      office_address: 'Kuleshwore' , 
-      office_email: 'ster@example.com', 
-      office_website:'www.ss.com')
-    Contact.new(first_name: 'Kinara', last_name: 'Maharjan')
-    office = contact.create_office_detail( 
-      office_name: 'Pioneer',
-      office_address: 'Tinkune' , 
-      office_email: 'es@work.com', 
-      office_website:'www.ss.com')
-    expect(office).to be_valid
-  end
 end
