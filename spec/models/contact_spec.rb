9693744a1019cc55f1029c0e47c3a5017743f717
NEHA SUWAl <neha@jyaasa.com>
2015-02-25 10:46:52 +0545

    test for returning contact's full name in a string

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Contact do
+    it "returns a contact's full name as a string" do
+      contact = Contact.new(first_name: 'Anil', last_name: 'Shrestha')
+      expect(contact.full_name).to eq 'Anil Shrestha'
+    end
+  end 
+
 end
