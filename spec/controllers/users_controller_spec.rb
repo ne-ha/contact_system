2c4712ef40d64d06bf1a2008f6a2e5f6244aa7b3
NEHA SUWAl <neha@jyaasa.com>
2015-02-26 13:42:59 +0545

    added test for when user is signed in and not signed in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,28 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  before :each do
-    User.create(:email => 'user@example.com' , :password => 'password')
+  # UsersController.skip_before_filter :authenticate_user!
+  
+  before :all do
+    User.destroy_all
+    @user = User.create(email: 'test@example.com', password: 'secretpassword')
+    contact = @user.contacts.create(first_name: 'Anil', last_name: 'Shakya')
   end
 
-  # describe "#index" do
-  #   it "should be success" do
-  #     get :index
-  #     expect(response).to be_success
-  #   end
-  # end
-  it "signs me in" do
-    visit "/users/sign_in"
-    within("#users") do
-      fill_in 'Email' , :with => 'user@example.com'
-      fill_in 'Password' , :with => 'password'
+  describe '#index' do
+    context "when user signs in" do
+      it "should render index template" do
+        sign_in :user, @user
+        get :index 
+        expect(response).to render_template("users/index")
+      end
     end
-    click_button 'Log in'
-    expect(page).to have_content 'Success'
-  end
 
+    context "when user is not signed in" do
+      it "should not render index template" do
+        get :index
+        expect(response).to redirect_to("/users/sign_in")
+      end
+    end
+  end
 end
